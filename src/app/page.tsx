50cb0c43f1a7d715bc32d3bfbe47af8a8ab5899e
Bernardo Freire <b.bernardo9815@gmail.com>
2024-11-12 10:18:33 -0300

    attualizacao da rota raiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Landing Page</h1>
-    </main>
-  );
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from './api/auth/[...nextauth]/route';
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    // Se o usuário está autenticado, redireciona para o dashboard
+    redirect('/app');
+  } else {
+    // Se não estiver autenticado, redireciona para a página de login
+    redirect('/auth');
+  }
+
+  return null;
 }
